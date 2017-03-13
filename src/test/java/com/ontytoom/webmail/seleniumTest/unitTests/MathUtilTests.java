5d8e76fde76ac326ab88f6406a31542090b0ad4f
onTy <onty.toom@gmail.com>
2017-03-13 09:57:51 -0400

    adding unit tests for utility classes MathUtil and StringUtil

--- src/test/java/com/ontytoom/webmail/seleniumTest/unitTests/MathUtilTests.java
+++ src/test/java/com/ontytoom/webmail/seleniumTest/unitTests/MathUtilTests.java
@@ -1,6 +1,9 @@
 package com.ontytoom.webmail.seleniumTest.unitTests;
 
+import com.ontytoom.webmail.seleniumTest.utils.MathUtil;
+
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Created by onTy on 2017-03-13.
@@ -11,7 +14,15 @@ public class MathUtilTests
 	@Test
 	public void testMakeRandomInt()
 	{
+		for ( int i = -1000; i < 1000; i++ )
+		{
+			int min = -1000;
+			int max = i;
 
+			int n = MathUtil.makeRandomInt( min, max );
+			assertTrue( n >= min );
+			assertTrue( n <= max );
+		}
 	}
 
 }
